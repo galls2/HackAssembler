2aafb5073e11e215b1fa8d09f5818e0bb7881d5c
galls2 <galls2@campus.technion.ac.il>
2023-05-15 22:51:05 +0300

    added symbol table impl

---
+++ code/symbol_table.cpp
@@ -0,0 +1,28 @@
+//
+// Created by USER on 15/05/2023.
+//
+
+#include "symbol_table.h"
+
+SymbolTable::SymbolTable() {
+    initialize_symbol_table();
+}
+
+bool SymbolTable::add_symbol(const std::string& symbol, const uint16_t value) {
+    return _table.emplace(symbol, value).second;
+}
+
+void SymbolTable::initialize_symbol_table() {
+    for (uint8_t i = 0; i <= s_max_logical_register_num; ++i)
+    {
+        _table.emplace("R"+std::to_string(i), i);
+    }
+
+    _table.emplace("SCREEN", s_screen_symbol_value);
+    _table.emplace("KBD", s_keyboard_symbol_value);
+    _table.emplace("SP", s_sp_symbol_value);
+    _table.emplace("LCL", s_lcl_symbol_value);
+    _table.emplace("ARG", s_arg_symbol_value);
+    _table.emplace("THIS", s_this_symbol_value);
+    _table.emplace("THAT", s_that_symbol_value);
+}
